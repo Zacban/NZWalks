3691a8404888311710385f26b8d08e4e05415bd7
Jesper Raundahl <zacban@gmail.com>
2022-11-04 22:43:49 +0100

    End of Section 3
    Implemented Async Controller and Repository on Regions

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller {
+        private readonly IRegionRepository regionRepository;
+
+        public RegionsController(IRegionRepository regionRepository, IMapper mapper) {
+            this.regionRepository = regionRepository;
+            Mapper = mapper;
+        }
+
+        public IMapper Mapper { get; }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions() {
+            var regions = await regionRepository.GetAllAsync();
+            var regionsDto = Mapper.Map<List<Models.DTO.Region>>(regions);
+            //var regionsDto = new List<Models.DTO.Region>();
+            //regions.ToList().ForEach(domainregion =>
+            //{
+            //    var regionDto = new Models.DTO.Region()
+            //    {
+            //        Id = domainregion.Id,
+            //        Area = domainregion.Area,
+            //        Code = domainregion.Code,
+            //        Latitude = domainregion.Latitude,
+            //        Longitude = domainregion.Longitude,
+            //        Name = domainregion.Name,
+            //        Population = domainregion.Population
+            //    };
+            //    regionsDto.Add(regionDto);
+
+            //});
+
+
+            return Ok(regionsDto);
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/DTO/Region.cs
@@ -0,0 +1,13 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Models.DTO {
+    public class Region {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Area { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+    }
+}
